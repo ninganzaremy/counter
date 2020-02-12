b9b8423e5a292a41149fdb406c07a50c595c687b
Ninganza Remy <remimaradona@gmail.com>
2020-02-11 21:50:53 -0500

    i added condition for incre and decr

--- src/App.js
+++ src/App.js
@@ -1,35 +1,80 @@
 import React, {Component} from 'react';
-class Counter extends Component {
-    constructor(){
 
-        super();
-        this.state ={
-            count: 0,
-        }
+class count extends Component {
+  constructor () {
+    super();
+    this.state = {
+      count: 0,
+      double: false,
     }
-    increment=() =>{
-        this.setState({
-            count: this.state.count + 1
-        })
-
-    };
+  }
 
 
-    render() {
-    return( //use () with multiple statements! Important
-      <div className ="container">
-           <div className ="navbar">Counter</div>
-           <div className= "counter">
-            <h1> {this.state.count}</h1>
-             <button type ="button" onClick={this.increment}>Increment</button>
-             <button type ="button"onClick={this.decrement}>Decrement</button>
-          </div>
+increment = () => {
+  if(this.state.count < 20) {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  if(this.state.double){
+    if(this.state.count < 19){
+      this.setState({
+        count: this.state.count + 2
+      })
+    }
+  }
+};
 
-      </div>
-
-    )
-
+decrement = () => {
+  if(this.state.count > 0) {
+    this.setState({
+      count: this.state.count -1,
+    })
+  }
+  if(this.state.double) {
+    if(this.state.count > 1) {
+      this.setState({
+        count: this.state.count -2
+      })
+    }
   }
 }
 
-export default Counter;
+clear = () => {
+  this.setState({
+    count: 0,
+  })
+}
+
+double = () => {
+  if(this.state.double) {
+    this.setState({
+      double: false,
+    })
+  } else {
+    this.setState({
+      double: true
+    })
+  }
+}
+
+
+  render() {
+    console.log("This is line 13 representating state", this.state.count);
+    console.log("This is the double value", this.state.double);
+    return(
+      <div className="container">
+        <div className="navbar">count.js</div>
+        <div className="count">
+          <h1>{this.state.count}</h1>
+          <button type="button" onClick={this.clear}>Clear Count</button>
+          <button type="button" onClick={this.increment}>Increment</button>
+          <button type="button" onClick={this.decrement}>Decrement</button>
+          
+        </div>
+      </div>
+    )
+  }
+}
+
+export default count;

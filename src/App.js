69daa72f9d39f3ffcfa2b98c05b29d16f9d549aa
Ninganza Remy <remimaradona@gmail.com>
2020-02-11 20:22:16 -0500

    added css and increment Method -tested and working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
-class HelloWorld extends Component {
-  render() {
-    return(
-      <h1> Hello Remy!!</h1>
-            
+class Counter extends Component {
+    constructor(){
+
+        super();
+        this.state ={
+            count: 0,
+        }
+    }
+    increment=() =>{
+        this.setState({
+            count: this.state.count + 1
+        })
+
+    };
+
+
+    render() {
+    return( //use () with multiple statements! Important
+      <div className ="container">
+           <div className ="navbar">Counter</div>
+           <div className= "counter">
+            <h1> {this.state.count}</h1>
+             <button type ="button" onClick={this.increment}>Increment</button>
+             <button type ="button"onClick={this.decrement}>Decrement</button>
+          </div>
+
+      </div>
+
     )
 
   }
 }
 
-export default HelloWorld;
+export default Counter;
